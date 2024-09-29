f9345c68333c914409ad1b84f2d46def1e0c45d8
KrishnaV2 <krishnavijaywargia007@gmail.com>
2024-09-30 02:15:19 +0530

    Proxy for render

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ const rateLimit = require('express-rate-limit')
 const path = require("path")
 
 const app = express();
+// Trust the first proxy
+app.set('trust proxy', 1); // You can also set it to true for trusting all proxies
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
